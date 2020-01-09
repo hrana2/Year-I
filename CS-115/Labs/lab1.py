73e091597f231bcbe06f9e771071bb6e4933385f
hrana2 <32397923+hrana2@users.noreply.github.com>
2020-01-08 22:03:18 -0500

    Add files via upload

---
+++ CS-115/Labs/lab1.py
@@ -0,0 +1,32 @@
+'''
+Created on Sep 7, 2017
+
+@author: himanshu_rana99
+Himanshu Rana 
+I pledge my honor that I have abided by the rules of the Stevens Honor System 
+'''
+
+from cs115 import map, reduce, range
+import math
+
+#The function returns the summation of a and b 
+def add(a, b):
+    return a + b
+
+#The function returns the reciprocal of n 
+def inverse(n):
+    return 1 / n
+
+#The function tries to evaluate the math number e by using Taylor expansion 
+def e(n):
+    return reduce(add, (map(inverse, map(math.factorial, range(n + 1)))))
+#The function returns the absolute value of the difference between the true value of e and e(n)
+def error(n):
+    return abs(math.e - e(n))
+
+
+
+
+
+
+    
